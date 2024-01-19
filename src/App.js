d87942b20b4817ee2e4aed9dfe2d961fb4a84d49
Chukwuebuka Ohiomoba <chukwuebukaohiomoba@chukwuebukasmbp.lan>
2024-01-18 16:04:10 -0800

    Initial user message

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit my portfolio <code>src/App.js</code> and save to reload.
+        Hi, My name is Chukwuebuka Ohiomoba 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/chukwuebuka-ohiomoba/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           I'm a Software Engineer. Check out my LinkedIn
         </a>
       </header>
     </div>
