ea13cf90880cf14db92d695a5dd1ff2a654835cb
Chukwuebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2024-02-05 23:44:05 -0800

    Fixed sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
@@ -5,7 +6,9 @@ const Layout = () => {
     return <>
     <Sidebar />
     <div className='page'>
-        <span className ='tags top-tags'></span>
+        <span className ='tags top-tags'>BODY</span>
+        <Outlet />
+        <span className='tags bottom-tags'>bottom</span>
     </div>
     </>
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,7 +2,7 @@ import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 import Ebuka from '../../assets/images/ebuka.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faAtom, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar = () => (
@@ -12,13 +12,16 @@ const Sidebar = () => (
 </Link>
 <nav>
     <NavLink exact ="true" activeclassname = "active" to="/">
-        <FontAwesomeIcon icon={faHome} color='4d4d4'></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faHome} color='#4d4d4e'></FontAwesomeIcon>
      </NavLink>
      <NavLink exact ="true" activeclassname = "active" className = "about-link" to="/about">
-        <FontAwesomeIcon icon={faUser} color='4d4d4'></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faUser} color='#4d4d4e'></FontAwesomeIcon>
      </NavLink>
      <NavLink exact ="true" activeclassname = "active" className = "contact-link" to="/contact">
-        <FontAwesomeIcon icon={faEnvelope} color='4d4d4'></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'></FontAwesomeIcon>
+     </NavLink>
+     <NavLink exact ="true" activeclassname = "active" className = "project-link" to="/project">
+        <FontAwesomeIcon icon={faAtom} color='#4d4d4e'></FontAwesomeIcon>
      </NavLink>
 </nav>
 </div>
